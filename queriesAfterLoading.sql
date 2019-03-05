ac4d5f1f21d6767c11bafb02e5911a6c5005ab8a
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-05 23:48:40 +0100

    Update queriesAfterLoading.sql

--- queriesAfterLoading.sql
+++ queriesAfterLoading.sql
@@ -1,19 +1,3 @@
-select , (select count(*) from courseregistrations where courseofferid = 1) as NumberOfRegStudents,
-	(select count(*) from studentassistants where courseofferid = 1) as NumberOfStudAssistants
-	from courseoffers
-	natural join courses
-	natural join degrees
-	natural join teacherassignmentstocourses
-	natural join teachers
-	where courseofferid = 1;
-
-select * , (select count(*) from courseregistrations where courseofferid = 1) as NumberOfRegStudents,
-	(select count(*) from studentassistants where courseofferid = 1) as NumberOfStudAssistants
-	from courseoffers
-	natural join courses
-	natural join degrees
-	natural join teacherassignmentstocourses
-	natural join teachers
-	where courseofferid = 1;
-
-select avg(grade) from courseregistrations where studentregistrationid = 140;
+SELECT * FROM CourseOffers, Courses, Degrees, Teachers WHERE CourseOffers.CourseOfferId = 1;
+SELECT StudentAssistants.CourseOfferId, CourseId, Year, Quartile, StudentId, StudentName, Address, BirthYearStudent, Gender, DegreeId, Dept, DegreeDescription, TotalECTS FROM StudentAssistants, CourseOffers, Students, Degrees WHERE StudentAssistents.StudenRegistrationId=140;
+SELECT AVG(Grade) FROM CourseRegistrations WHERE CourseRegistrations.StudentRegistrationId=140;
