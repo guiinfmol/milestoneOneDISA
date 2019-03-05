c465d8a8677367f6ff9329e6c96ddabf69120785
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-06 00:10:46 +0100

    Update queriesAfterLoading.sql

--- queriesAfterLoading.sql
+++ queriesAfterLoading.sql
@@ -1,3 +1,3 @@
-SELECT * FROM CourseOffers, Courses, Degrees, Teachers WHERE CourseOffers.CourseOfferId = 1;
-SELECT StudentAssistants.CourseOfferId, CourseId, Year, Quartile, StudentId, StudentName, Address, BirthYearStudent, Gender, DegreeId, Dept, DegreeDescription, TotalECTS FROM StudentAssistants, CourseOffers, Students, Degrees WHERE StudentAssistents.StudenRegistrationId=140;
+SELECT 0;
+SELECT 0;
 SELECT AVG(Grade) FROM CourseRegistrations WHERE CourseRegistrations.StudentRegistrationId=140;
