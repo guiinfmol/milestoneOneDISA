f7d638107091d14a50b67f2e4de4fb79997b12fc
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-06 22:31:59 +0100

    Update loadData.sql

--- loadData.sql
+++ loadData.sql
@@ -1,1 +1,9 @@
-
+\copy Degrees FROM '/mnt/ramdisk/tables/Degrees.table' DELIMITER ',' CSV HEADER;
+\copy Students FROM '/mnt/ramdisk/tables/Students.table' DELIMITER ',' CSV HEADER;
+\copy StudentRegistrationsToDegrees FROM '/mnt/ramdisk/tables/StudentRegistrationsToDegrees.table' DELIMITER ',' CSV HEADER;
+\copy Teachers FROM '/mnt/ramdisk/tables/Teachers.table' DELIMITER ',' CSV HEADER;
+\copy Courses FROM '/mnt/ramdisk/tables/Courses.table' DELIMITER ',' CSV HEADER;
+\copy CourseOffers FROM '/mnt/ramdisk/tables/CourseOffers.table' DELIMITER ',' CSV HEADER;
+\copy TeacherAssignmentsToCourses FROM '/mnt/ramdisk/tables/TeacherAssignmentsToCourses.table' DELIMITER ',' CSV HEADER;
+\copy StudentAssistants FROM '/mnt/ramdisk/tables/StudentAssistants.table' DELIMITER ',' CSV HEADER;
+\copy CourseRegistrations FROM '/mnt/ramdisk/tables/CourseRegistrations.table' DELIMITER ',' CSV HEADER NULL AS 'null';
