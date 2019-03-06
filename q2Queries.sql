05c2884a44abbd770b0b1c64fd79de3cb06582bb
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-06 21:35:25 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,8 +1,8 @@
-select 0;
-select 0;
-select 0;
-select 0;
-select 0;
-select 0;
-SELECT degreeid, birthyearstudent, gender, 1.0*sum(ects*grade)/sum(ects) as avgGrade FROM students natural join studentregistrationstodegrees natural join courseregistrations natural join courseoffers natural join courses GROUP BY CUBE(degreeid, birthyearstudent, gender) ORDER BY degreeid, birthyearstudent, gender;
-select 0;
+SELECT Courses.CourseName, CourseOfferRegistrations.Grade FROM StudentRegistrationsToDegrees, CourseOfferRegistrations, Courses WHERE StudentRegistrationsToDegrees.StudentId = %1% AND StudentRegistrationsToDegrees.DegreeId = %2% AND CourseOfferRegistrations.StudentRegistrationId = StudentRegistrationsToDegrees.StudentRegistrationId AND CourseOfferRegistrations.CourseId = Courses.CourseId AND CourseOfferRegistrations.Grade >= 5 ORDER BY CourseOfferRegistrations.Year, CourseOfferRegistrations.Quartile, CourseOfferRegistrations.CourseOfferId;
+SELECT 0;
+SELECT 0;
+SELECT 0;
+SELECT 0;
+SELECT 0;
+SELECT 0;
+SELECT 0;
