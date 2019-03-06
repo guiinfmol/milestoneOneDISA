9da9f28c4d255eae7419363ec593279edb3ae305
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-06 19:46:04 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,8 +1,8 @@
 select 0;
 select 0;
 select 0;
-select 1.0*sum(case when gender='F' THEN 1 ELSE 0 END)/count(*) as percentage from studentregistrationstodegrees natural join students natural join degrees where dept = %1%
+select 1.0*sum(case when gender='F' THEN 1 ELSE 0 END)/count(*) as percentage from studentregistrationstodegrees natural join students natural join degrees where dept = %1%;
 select 0;
 select 0;
-select 0;
+SELECT degreeid, birthyearstudent, gender, 1.0*sum(ects*grade)/sum(ects) as avgGrade FROM students natural join studentregistrationstodegrees natural join courseregistrations natural join courseoffers natural join courses GROUP BY CUBE(degreeid, birthyearstudent, gender) ORDER BY degreeid, birthyearstudent, gender;
 select 0;
