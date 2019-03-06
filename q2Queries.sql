4c39f6bbfcb18e5c4ed4fe9d79f776234b0c2a91
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-06 21:42:26 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,4 +1,3 @@
-SELECT Courses.CourseName, CourseOfferRegistrations.Grade FROM StudentRegistrationsToDegrees, CourseOfferRegistrations, Courses WHERE StudentRegistrationsToDegrees.StudentId = %1% AND StudentRegistrationsToDegrees.DegreeId = %2% AND CourseOfferRegistrations.StudentRegistrationId = StudentRegistrationsToDegrees.StudentRegistrationId AND CourseOfferRegistrations.CourseId = Courses.CourseId AND CourseOfferRegistrations.Grade >= 5 ORDER BY CourseOfferRegistrations.Year, CourseOfferRegistrations.Quartile, CourseOfferRegistrations.CourseOfferId;
 SELECT 0;
 SELECT 0;
 SELECT 0;
