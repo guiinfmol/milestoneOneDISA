b8846027b0426f6d9a942e4788e454b1a3e091c6
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-06 16:36:50 +0100

    Update loadData.sql

--- loadData.sql
+++ loadData.sql
@@ -8,19 +8,3 @@ COPY TeacherAssignmentsToCourses(CourseOfferId, TeacherId) FROM '/mnt/ramdisk/ta
 COPY StudentAssistants(CourseOfferId, StudentRegistrationId) FROM '/mnt/ramdisk/tables/StudentAssistants.table' DELIMITER ',' CSV HEADER;
 COPY CourseRegistrations(CourseOfferId, StudentRegistrationId, Grade) FROM '/mnt/ramdisk/tables/CourseRegistrations.table' NULL 'null' DELIMITER ',' CSV HEADER;
 ANALYZE VERBOSE;
--- Degrees
---ALTER TABLE Degrees ADD PRIMARY KEY(DegreeId);
---Students
---ALTER TABLE Students ADD PRIMARY KEY(StudentId);
---StudentRegistrationsToDegrees
---ALTER TABLE StudentRegistrationsToDegrees ADD PRIMARY KEY(StudentRegistrationId);
---Teachers
---ALTER TABLE Teachers ADD PRIMARY KEY(TeacherId);
---Courses
---ALTER TABLE Courses ADD PRIMARY KEY(CourseId);
---CourseOffers
---ALTER TABLE CourseOffers ADD PRIMARY KEY(CourseOfferId);
---CourseRegistrations
---ALTER TABLE CourseRegistrations ADD PRIMARY KEY(StudentRegistrationId, CourseOfferId);
---StudentAssistants
---ALTER TABLE StudentAssistants ADD PRIMARY KEY(CourseOfferId, StudentRegistrationId);
