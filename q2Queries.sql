6666579cc629901b51d761b5a323137f717bba50
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-06 13:28:28 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,5 +1,5 @@
-select coursename, grade from (courseoffers co natural join courseregistrations cr natural join studentregistrationstodegrees srtd) inner join courses c on co.courseid = c.courseid where studentid = %1% and srtd.degreeid = %2% AND grade >= 5 ORDER BY year, quartile, courseofferid;
-select studentid from PassedCreditsPerRegistration pcpr NATURAL JOIN STUDENTregistrationstodegrees where passedCredits  >= (select totalECTS from degrees natural join studentregistrationstodegrees where StudentRegistrationId = pcpr.StudentRegistrationId) AND (SELECT COUNT(*) FROM courseregistrations where grade < 5 and pcpr.studentregistrationid = studentregistrationid) = 0 AND (avgMark) > %1% ORDER BY studentid;
+select 0;
+select 0;
 select 0;
 select 0;
 select 0;
