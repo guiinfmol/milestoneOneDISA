d889b3fd44d0d9d36644a9f94216cc0f73f470e6
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-06 21:40:57 +0100

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -1,9 +1,1 @@
-CREATE UNLOGGED TABLE Degrees(DegreeId int, Dept varchar(50), DegreeDescription varchar(200), TotalECTS smallint);
-CREATE UNLOGGED TABLE Students(StudentId int, StudentName varchar(50), Address varchar(200), BirthyearStudent smallint, Gender char);   
-CREATE UNLOGGED TABLE StudentRegistrationsToDegrees(StudentRegistrationId int, StudentId int, DegreeId int, RegistrationYear smallint);
-CREATE UNLOGGED TABLE Teachers(TeacherId int, TeacherName varchar(50), Address varchar(200), BirthyearTeacher smallint, Gender char);
-CREATE UNLOGGED TABLE Courses(CourseId int, CourseName varchar(50), CourseDescription varchar(200), DegreeId int, ECTS smallint);
-CREATE UNLOGGED TABLE CourseOffers(CourseOfferId int, CourseId int, Year smallint, Quartile smallint);
-CREATE UNLOGGED TABLE TeacherAssignmentsToCourses(CourseOfferId int, TeacherId int);
-CREATE UNLOGGED TABLE StudentAssistants(CourseOfferId int, StudentRegistrationId int);
-CREATE UNLOGGED TABLE CourseRegistrations(CourseOfferId int, StudentRegistrationId int, Grade smallint);
+
