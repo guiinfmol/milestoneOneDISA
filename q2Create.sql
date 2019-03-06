1b4bc82acd60e9f2605c1fe7df724e62ab02e255
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-06 20:26:22 +0100

    Update q2Create.sql

--- q2Create.sql
+++ q2Create.sql
@@ -1,9 +1,1 @@
-ALTER TABLE Degrees ADD PRIMARY KEY(DegreeId);
-ALTER TABLE Students ADD PRIMARY KEY(StudentId);
-ALTER TABLE StudentRegistrationsToDegrees ADD PRIMARY KEY(StudentRegistrationId);
-ALTER TABLE Teachers ADD PRIMARY KEY(TeacherId);
-ALTER TABLE Courses ADD PRIMARY KEY(CourseId);
-ALTER TABLE CourseOffers ADD PRIMARY KEY(CourseOfferId);
-ALTER TABLE CourseRegistrations ADD PRIMARY KEY(StudentRegistrationId, CourseOfferId);
-ALTER TABLE StudentAssistants ADD PRIMARY KEY(CourseOfferId, StudentRegistrationId);
-CREATE INDEX idx_srtd_on_courseregistrations ON courseregistrations(studentregistrationid);
+
