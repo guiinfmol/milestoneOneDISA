b6c8d0dde467132be0d07ee53d42932f1b96cf6d
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-06 16:31:27 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,7 +1,7 @@
 select 0;
 select 0;
 select 0;
-select 1.0*sum(case when gender='F' THEN 1 ELSE 0 END)/count(*)) as percentage from studentregistrationstodegrees natural join students natural join degrees where dept = %1%
+select 1.0*sum(case when gender='F' THEN 1 ELSE 0 END)/count(*) as percentage from studentregistrationstodegrees natural join students natural join degrees where dept = %1%
 select 0;
 select 0;
 select 0;
