3f5aa7b7adb328e596e715afacd1ddac212eb3b4
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-06 22:37:05 +0100

    Update q2Create.sql

--- q2Create.sql
+++ q2Create.sql
@@ -1,1 +1,8 @@
-
+DROP TABLE Teachers;
+DROP TABLE TeacherAssignmentsToCourses;
+ALTER TABLE Degrees DROP COLUMN DegreeDescription;
+ALTER TABLE Students DROP COLUMN Address;
+ALTER TABLE Students DROP COLUMN StudentName;
+ALTER TABLE Courses DROP COLUMN CourseDescription;
+ANALYZE VERBOSE;
+CREATE INDEX idx_srtd_on_courseregistrations ON courseregistrations(studentregistrationid);
