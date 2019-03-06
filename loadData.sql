6d8bf81916d9ab90f387a83054300039366fc5e0
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-06 12:40:27 +0100

    Update loadData.sql

--- loadData.sql
+++ loadData.sql
@@ -1,12 +1,12 @@
-COPY Degrees FROM '/mnt/ramdisk/tables/Degrees.table' DELIMITER ',' CSV HEADER;
-COPY Students FROM '/mnt/ramdisk/tables/Students.table' DELIMITER ',' CSV HEADER;
-COPY StudentRegistrationsToDegrees FROM '/mnt/ramdisk/tables/StudentRegistrationsToDegrees.table' DELIMITER ',' CSV HEADER;
-COPY Teachers FROM '/mnt/ramdisk/tables/Teachers.table' DELIMITER ',' CSV HEADER;
-COPY Courses FROM '/mnt/ramdisk/tables/Courses.table' DELIMITER ',' CSV HEADER;
-COPY CourseOffers FROM '/mnt/ramdisk/tables/CourseOffers.table' DELIMITER ',' CSV HEADER;
-COPY TeacherAssignmentsToCourses FROM '/mnt/ramdisk/tables/TeacherAssignmentsToCourses.table' DELIMITER ',' CSV HEADER;
-COPY StudentAssistants FROM '/mnt/ramdisk/tables/StudentAssistants.table' DELIMITER ',' CSV HEADER;
-COPY CourseRegistrations FROM '/mnt/ramdisk/tables/CourseRegistrations.table' DELIMITER ',' CSV HEADER NULL AS 'null';
+COPY Degrees FROM '/mnt/ramdisk/tables/Degrees.txt' DELIMITER ',' CSV HEADER;
+COPY Students FROM '/mnt/ramdisk/tables/Students.txt' DELIMITER ',' CSV HEADER;
+COPY StudentRegistrationsToDegrees FROM '/mnt/ramdisk/tables/StudentRegistrationsToDegrees.txt' DELIMITER ',' CSV HEADER;
+COPY Teachers FROM '/mnt/ramdisk/tables/Teachers.txt' DELIMITER ',' CSV HEADER;
+COPY Courses FROM '/mnt/ramdisk/tables/Courses.txt' DELIMITER ',' CSV HEADER;
+COPY CourseOffers FROM '/mnt/ramdisk/tables/CourseOffers.txt' DELIMITER ',' CSV HEADER;
+COPY TeacherAssignmentsToCourses FROM '/mnt/ramdisk/tables/TeacherAssignmentsToCourses.txt' DELIMITER ',' CSV HEADER;
+COPY StudentAssistants FROM '/mnt/ramdisk/tables/StudentAssistants.txt' DELIMITER ',' CSV HEADER;
+COPY CourseRegistrations FROM '/mnt/ramdisk/tables/CourseRegistrations.txt' DELIMITER ',' CSV HEADER NULL AS 'null';
 -- Degrees
 ALTER TABLE Degrees ADD PRIMARY KEY(DegreeId);
 --Students
