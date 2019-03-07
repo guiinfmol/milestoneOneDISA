652e05818ed433aa58b1d6f7e3ce018b03580598
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-07 22:57:10 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,5 +1,4 @@
-select coursename, grade from courseoffers natural join courseregistrations natural join studentregistrationstodegrees natural join courses where studentid = %1% and degreeid = %2% AND grade >= 5 ORDER BY year, quartile, courseofferid;
-SELECT 0;
+select 0;
 SELECT 0;
 SELECT 0;
 SELECT 0;
