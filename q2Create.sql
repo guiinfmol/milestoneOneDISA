0b660c4de9a9b943d6c76fbf96e111c963faa314
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-08 16:24:29 +0100

    Update q2Create.sql

--- q2Create.sql
+++ q2Create.sql
@@ -1,10 +1,8 @@
---DROP TABLE Teachers;
---DROP TABLE TeacherAssignmentsToCourses;
---ALTER TABLE Degrees DROP COLUMN DegreeDescription;
---ALTER TABLE Students DROP COLUMN Address;
---ALTER TABLE Students DROP COLUMN StudentName;
---ALTER TABLE Courses DROP COLUMN CourseDescription;
---ANALYZE VERBOSE;
 CREATE INDEX idx_srtd_on_courseregistrations ON courseregistrations(studentregistrationid);
---CREATE MATERIALIZED VIEW PassedCreditsPerRegistration(studentregistrationid, passedCredits, avgMark) AS SELECT StudentRegistrationId, sum(ects) passedCredits, cast(1.0*sum(ects*grade)/sum(ects) as decimal(10,2)) as AvgMark FROM courseregistrations d natural join courseoffers natural join courses WHERE grade >= 5 GROUP BY StudentRegistrationId;
---CREATE MATERIALIZED VIEW ActiveStudents(studentregistrationid) AS select studentregistrationid FROM PassedCreditsPerRegistration d natural join studentregistrationstodegrees natural join degrees where totalects <= passedcredits;
+CREATE MATERIALIZED VIEW PassedCreditsPerRegistration(studentregistrationid, passedCredits, avgMark) AS SELECT StudentRegistrationId, sum(ects) passedCredits, cast(1.0*sum(ects*grade)/sum(ects) as decimal(10,2)) as AvgMark FROM courseregistrations d natural join courseoffers natural join courses WHERE grade >= 5 GROUP BY StudentRegistrationId;
+CREATE MATERIALIZED VIEW ActiveStudents(studentregistrationid) AS select studentregistrationid FROM PassedCreditsPerRegistration d natural join studentregistrationstodegrees natural join degrees where totalects <= passedcredits;
+CREATE INDEX idx_srid_activestudents ON ActiveStudents(studentregistrationid);
+CREATE MATERIALIZED VIEW MaxGradesOfferAndStudent(courseofferid, grade, studentid) AS SELECT courseofferid, grade, studentid FROM (SELECT courseofferid, max(grade) from courseregistrations natural join courseoffers where year = 2018 and quartile = 1 GROUP BY courseofferid) AS FOO NATURAL JOIN COURSEREGISTRATIONS cr NATURAL JOIN studentregistrationstodegrees WHERE foo.max = cr.grade;
+CREATE INDEX idx_sid_maxgrades ON MaxGradesOfferAndStudent(studentid);
+CREATE VIEW studentsCourseOfExcellence(studentid, numberCourses) AS SELECT STUDENTID, COUNT(*) as numberCourses FROM MaxGradesOfferAndStudent GROUP BY STUDENTID;
+ANALYZE;
