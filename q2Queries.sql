b15456bc140ba3383b71b82ec0b19d0087d3a04b
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-08 01:12:24 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,7 +1,7 @@
 select coursename, grade from courseoffers natural join courseregistrations natural join studentregistrationstodegrees natural join courses where studentid = %1% and degreeid = %2% AND grade >= 5 ORDER BY year, quartile, courseofferid;
-SELECT 0;
-SELECT 0;
-SELECT 0;
+select studentid from PassedCreditsPerRegistration pcpr NATURAL JOIN STUDENTregistrationstodegrees where passedCredits  >= (select totalECTS from degrees natural join studentregistrationstodegrees where StudentRegistrationId = pcpr.StudentRegistrationId) AND (SELECT COUNT(*) FROM courseregistrations where grade < 5 and pcpr.studentregistrationid = studentregistrationid) = 0 AND (avgMark) > %1% ORDER BY studentid;
+select degreeid, 1.0*sum(case when gender='F' THEN 1 ELSE 0 END)/count(*) as percentage from students s natural join studentregistrationstodegrees where EXISTS (select * from ActiveStudents where studentid = s.studentid) GROUP BY degreeid ORDER BY degreeid;
+select (1.0*sum(case when gender='F' THEN 1 ELSE 0 END)/count(*)) as percentage from studentregistrationstodegrees natural join students natural join degrees where dept = 'standard a';
 SELECT 0;
 SELECT 0;
 SELECT 0;
