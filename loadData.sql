fee5ab55e241e2f3fdf6875fb098fa657f2630df
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-08 16:23:22 +0100

    Update loadData.sql

--- loadData.sql
+++ loadData.sql
@@ -7,4 +7,8 @@ COPY CourseOffers(CourseOfferId, CourseId, Year, Quartile) FROM '/mnt/ramdisk/ta
 COPY TeacherAssignmentsToCourses(CourseOfferId, TeacherId) FROM '/mnt/ramdisk/tables/TeacherAssignmentsToCourses.table' DELIMITER ',' CSV HEADER;
 COPY StudentAssistants(CourseOfferId, StudentRegistrationId) FROM '/mnt/ramdisk/tables/StudentAssistants.table' DELIMITER ',' CSV HEADER;
 COPY CourseRegistrations(CourseOfferId, StudentRegistrationId, Grade) FROM '/mnt/ramdisk/tables/CourseRegistrations.table' DELIMITER ',' CSV HEADER NULL AS 'null';
-ANALYZE VERBOSE;
+ALTER TABLE Degrees ADD PRIMARY KEY(DegreeId);
+ALTER TABLE Students ADD PRIMARY KEY(StudentId);
+ALTER TABLE StudentRegistrationsToDegrees ADD PRIMARY KEY(StudentRegistrationId);
+ALTER TABLE Courses ADD PRIMARY KEY(CourseId);
+ALTER TABLE CourseOffers ADD PRIMARY KEY(CourseOfferId);
