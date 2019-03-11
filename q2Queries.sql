67e6784c92ac540d7548927feaf4f74b9a28ad72
guiinfmol <39101921+guiinfmol@users.noreply.github.com>
2019-03-11 20:28:42 +0100

    version B2 Q7

--- q2Queries.sql
+++ q2Queries.sql
@@ -4,5 +4,5 @@ select 0;
 select 0;
 select 0;
 select 0;
-SELECT sr.degreeid, birthyearstudent, gender, avg(avgMark)::float as avgGrade FROM students s, passedcreditsperregistration pc, studentregistrationstodegrees sr, activeregistrations ar, courseregistrations cr, courseoffers co, courses c WHERE co.courseid = c.courseid AND co.courseofferid = cr.courseofferid  AND cr.studentregistrationid = sr.studentregistrationid AND ar.studentregistrationid = pc.studentregistrationid AND sr.studentid = s.studentid AND sr.studentregistrationid = pc.studentregistrationid GROUP BY CUBE(sr.degreeid, birthyearstudent, gender)  ORDER BY sr.degreeid, birthyearstudent, gender;
+SELECT degreeid, birthyearstudent, gender, avg(avgMark)::float as avgGrade FROM students s natural join passedcreditsperregistration pc natural join studentregistrationstodegrees sr natural join activeregistrations ar natural join courseregistrations cr natural join courseoffers co natural join courses c GROUP BY CUBE(sr.degreeid, birthyearstudent, gender)  ORDER BY sr.degreeid, birthyearstudent, gender;
 select coursename, year, quartile FROM courses c, courseoffers co, CourseOffersRegsAss cora where 1.0*regs/50 > ass AND c.courseid = co.courseid AND co.courseofferid = cora.courseofferid order by cora.courseofferid;
